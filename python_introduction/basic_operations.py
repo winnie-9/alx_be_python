bfd5b6699c2f808c109d30c4602393d40e17a443
winnie-9 <winniekendra540@gmail.com>
2024-06-22 09:58:36 +0000

    done committing

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,4 +1,5 @@
 #!bin/bash
 number1 = 10
 number2 =  5
+Calculate the sum, difference (by subtracting number2 from number1), and product of number1 and number2.
 [operation] of [number1] and [number2] is [result]
