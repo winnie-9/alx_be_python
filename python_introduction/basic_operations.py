16fe24c4c7a6bd0b6e42034cba1a67811bf816b1
winnie-9 <winniekendra540@gmail.com>
2024-06-22 11:37:39 +0000

    done

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,1 +1,8 @@
-
+number1=10
+number2=5
+sum=number1 + number2
+difference = number1 - number2
+product = number1 * number2
+print("Addition of 10 and 5 is", sum)
+print("Subtraction of 10 and 5 is" ,difference)
+print("Multiplication of 10 and 5 is" ,product)
