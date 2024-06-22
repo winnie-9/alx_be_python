39df6fef45f4109fa9ff078cfc5977631c6ab546
winnie-9 <winniekendra540@gmail.com>
2024-06-22 10:56:49 +0000

    done

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,6 +1,6 @@
 #!bin/bash
 number1 = 10 number2 = 5
-print(10+5)
-print(10-5)
-print(10*5)
+print(number1+number2)
+print(number1-number2)
+print(number1*number2)
 [operation] of [number1] and [number2] is [result]
