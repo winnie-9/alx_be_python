9c196213ba6106503f0b714bd85c0b2b7dd65aae
winnie-9 <winniekendra540@gmail.com>
2024-06-29 16:49:26 +0000

    weather

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,7 +1,7 @@
 #! bin/bash
 weather = input("what's the weather like today? (sunny/rainy/cold):")
-if weather == "sunny" :recommendation = "Wear a t-shirt and sunglasses."
-elif weather == "rainy" :recommendation = "Don't forget your umbrella and a raincoat."
-elif weather == "cold" :recommendation = "Make sure to wear a warm coat and a scarf."
+if "sunny" :recommendation = "Wear a t-shirt and sunglasses."
+elif "rainy" :recommendation = "Don't forget your umbrella and a raincoat."
+elif "cold" :recommendation = "Make sure to wear a warm coat and a scarf."
 else:recommendation = "Sorry, I don't have recommendations for this weather."
 print(recommendation)
