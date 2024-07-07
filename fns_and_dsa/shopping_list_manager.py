440a3134c5b33ef629930d92362aca2f04dcd3fe
winnie-9 <winniekendra540@gmail.com>
2024-07-07 01:30:49 +0000

    done

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -1,4 +1,11 @@
 shopping_list = []
+def display_menu():
+    print("\nShopping List Menu:")
+    print("1. Add an item")
+    print("2. Remove an item")
+    print("3. View the list")
+    print("4. Exit")
+
 def add_item(shopping_list):
     item = input("Enter the name of the item to add: ").strip()
     shopping_list.append(item)
