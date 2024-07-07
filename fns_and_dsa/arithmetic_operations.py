5bccb5271fb0938737a11fe199d7a7be5323438d
winnie-9 <winniekendra540@gmail.com>
2024-07-07 22:00:53 +0000

    operations

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,21 @@
+def perform_operation(num1, num2, operation):
+
+        
+        operation = input("Enter operation:(add,subtract,multiply,divide) ").lower()
+        num1 = float(input("Enter the first number: "))
+        num2 = float(input("Enter the second number: "))
+        if operation == 'add':
+            return num1 + num2
+        elif operation == 'subtract':
+            return num1 - num2
+        elif operation == 'multiply':
+                return num1 * num2
+        elif operation == 'divide':
+            if num2 == 0:
+                return "You cannot divide by zero!"
+            else:
+                return num1 / num2
+        else:
+                 return "Error: Invalid operation"
+result = perform_operation(1,1 , 'add')
+print(f"Result: {result}")
